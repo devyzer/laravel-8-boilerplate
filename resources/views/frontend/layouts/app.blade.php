615c458c012726c19118882bc959f61860d579c6
Ahmad <ahmad.alhourani@xlabtechs.com>
2019-04-22 22:07:22 +0300

    update lang

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
-
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
+@if (app()->getLocale()=="ar")
+    <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="rtl">--}}
+@else
+    <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@endif
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
